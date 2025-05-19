875b323f95035c3868e60a5113d8f7cac8cefaba
Sarada Prasanna Dash <dashsharada@gmail.com>
2025-05-19 23:17:25 +0530

    completed addding video and image uploader

---
+++ app/api/videos/route.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@/generated/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient()
+
+export async function GET(request: NextRequest){
+    try{
+        const videos=await prisma.video.findMany({
+            orderBy:{createdAt:"desc"}
+            
+        })
+        return NextResponse.json(videos)
+    }
+    catch (error){
+        console.log(error)
+        return NextResponse.json({error:"Error fetching videos"},
+            {status:500}
+        )
+    }
+    finally{
+        await prisma.$disconnect()
+    }
+}
